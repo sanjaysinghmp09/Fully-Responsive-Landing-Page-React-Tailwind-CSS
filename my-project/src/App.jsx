cd1cabce6e65618b4bb4857894494ba071541d6a
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-10-28 20:35:46 +0530

    setup

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div> <h1 class="text-3xl font-bold underline bg-red-400">
-    Hello world!
-  </h1></div>
+   <>
+    <Navbar/>
+   </> 
   )
 }
 
---
+++ my-project/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-neutral-700/80">
+
+        <div className="container px-4 mx-auto relative text-sm">
+            <div className="flex justify-center items-center">
+                <div className="flex items-center shrink-0">
+
+                </div>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
