39e98581ce936aa3d7a1254069273c6e2758f0f4
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-10-29 21:34:13 +0530

    Feature Section complited

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import FeatureSection from './components/FeatureSection'
+import WorkFlow from './components/WorkFlow'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
@@ -11,6 +16,10 @@ function App() {
  <HeroSection/>
     </div>
     <FeatureSection/>
+    <WorkFlow/>
+    <Pricing/>
+    <Testimonials/>
+    <Footer/>
    
 
 
