ad3e184281983119cd53eff1a74a70b67bca445b
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-10-29 12:34:08 +0530

    setup

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
 
 function App() {
   return (
    <>
     <Navbar/>
+    <div className="max-w-7xl mx-auto pt-20 px-6">
+ <HeroSection/>
+    </div>
+   
+
+
    </> 
   )
 }
---
+++ my-project/src/components/HeroSection.jsx
@@ -0,0 +1,16 @@
+
+
+
+function HeroSection() {
+    return (
+        <div className="flex flex-col items-center mt-6 lg:mt20">
+            <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">VirtualX build tools<span className="text-transparent bg-clip-text bg-linear-to-r from-orange-500 to-orange-800"> for developers</span>
+            </h1>
+            <p className="mt-6 text-lg text-center text-gray-500 max-w-3xl">
+                VirtualX helps developers build, test, and launch amazing digital products faster with powerful tools and seamless collaboration.
+            </p>
+        </div>
+    )
+}
+
+export default HeroSection
