17e2a6378aa671d75bcfa1f934bf725b9d02727a
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-10-29 12:40:41 +0530

    Hero Section complited

--- my-project/src/components/HeroSection.jsx
+++ my-project/src/components/HeroSection.jsx
@@ -9,6 +9,10 @@ function HeroSection() {
             <p className="mt-6 text-lg text-center text-gray-500 max-w-3xl">
                 VirtualX helps developers build, test, and launch amazing digital products faster with powerful tools and seamless collaboration.
             </p>
+            <div className="flex justify-center my-10">
+                <a href="#" className="bg-linear-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md">Start For Free</a>
+                <a href="" className="py-3 px-4 mx-3 rounded-md border">Documentation</a>
+            </div>
         </div>
     )
 }
